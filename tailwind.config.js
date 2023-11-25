192b526b96b14dff3c196b97e02261986f2b4d7c
Franco Mendez <125481413+francomendezok@users.noreply.github.com>
2023-11-25 13:52:57 -0300

    PC Layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ module.exports = {
   content: ['./build/**/*.html'],
   theme: {
     screens: {
-      'xs': '320px'
+      'xs': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
     },
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 }
 
